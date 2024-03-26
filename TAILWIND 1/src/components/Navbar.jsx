22bfd5f1256c7c2b16a661f0d01ce4b9b6c7cc43
faithyife <faithyuchechi@gmail.com>
2024-03-27 00:05:35 +0100

    real_estate

---
+++ TAILWIND 1/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <>
+    <div className="navbar bg-blue-400 h-10 text-white">
+  <div className="flex-1">
+  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"/>
+</svg>
+<p className="btn btn-ghost text-x">Rezilla, 18 Gratton st, Brooklyn</p>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>
+            Parent
+          </summary>
+          <ul className="p-2 bg-base-100 rounded-t-none">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+    </>
+  );
+};
+
+export default Navbar;
